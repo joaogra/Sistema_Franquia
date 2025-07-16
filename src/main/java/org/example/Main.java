d01be91c12f049bfe9a32466ba976d996cfaa3dc
joaogra <joao.granatto@estudante.ufjf.br>
2025-07-16 16:26:13 -0300

    inicia projeto1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Inicia projeto");
+        System.out.println("Teste 01");
     }
 }

4f2bd38cab0123af1f6d7a10f9821fe3a7b5b975
joaogra <joao.granatto@estudante.ufjf.br>
2025-07-25 21:54:10 -0300

    comeca tela de cadastro de franquia

---
+++ src/main/java/view/TelaFranquia.java
@@ -0,0 +1,21 @@
+package view;
+
+import javax.swing.*;
+
+public class TelaFranquia {
+    private JPanel panel1;
+    private JButton cancelarButton;
+    private JButton adicionarButton;
+    private JButton voltarButton;
+    private JTextField textField1;
+    private JPasswordField ruaTxt;
+    private JTextField bairroTxt;
+    private JTextField numeroTxt;
+    private JTextField complementoTxt;
+    private JTextField cidadeTxt;
+    private JTextField estadoTxt;
+    private JTextField cepTxt;
+    private JButton buscarBtn;
+
+
+}

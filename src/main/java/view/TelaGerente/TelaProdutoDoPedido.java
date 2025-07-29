d4375bb75572d9adb9e7f22ac992416b155bb671
adrianm07 <adrianmaciel007@gmail.com>
2025-07-29 20:21:29 -0300

    tela de visualizaçao de pedidos do gerente

---
+++ src/main/java/view/TelaGerente/TelaProdutoDoPedido.java
@@ -0,0 +1,43 @@
+package view.TelaGerente;
+
+import controller.gerente.PedidoController;
+import model.Pedido;
+import model.Pessoas.Gerente;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+
+public class TelaProdutoDoPedido extends JDialog {
+    private JPanel painelProdutoPedido;
+    private JTable tabelaProdutos;
+    private JLabel titulo;
+    private JButton fecharBtn;
+    private PedidoController pedidoController;
+    public TelaProdutoDoPedido(JDialog parent, Gerente gerente, Pedido pedido){
+        super(parent,"Produtos",true);
+        setContentPane(painelProdutoPedido);
+        setSize(800,600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        pedidoController = new PedidoController(gerente);
+
+        String[] colunas = {"Codigo","Nome","Quantidade","Preço"};
+        DefaultTableModel tabela = new DefaultTableModel(colunas,0){
+            @Override public boolean isCellEditable(int linha, int coluna) {
+                return false;
+            }
+        };
+        List<Object[]> produtos = pedidoController.listaProdutosUnicoPedido(pedido);
+        for(Object[] produto : produtos){
+            tabela.addRow(produto);
+        }
+        tabelaProdutos.setModel(tabela);
+        tabelaProdutos.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        fecharBtn.addActionListener(e -> {
+           this.dispose();
+        });
+    }
+}
